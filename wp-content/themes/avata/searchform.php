093aedbfadf583529fc69c6e4b56d9b39baa39f1
Kartik B <kartikbhalla12@gmail.com>
2017-12-21 14:57:01 +0000

    Automated Backup 21-12-2017
    Signed-off-by: Kartik B <kartikbhalla12@gmail.com>

--- wp-content/themes/avata/searchform.php
+++
@@ -1,6 +0,0 @@
-<form role="search" method="get" class="search-form" action="<?php echo esc_url(home_url('/'));?>">
-  <label> <span class="screen-reader-text"><?php _e("Search for","avata");?>:</span>
-    <input type="search" class="search-field" placeholder="<?php esc_attr_e("Search","avata");?>&hellip;" value="<?php the_search_query(); ?>" name="s">
-  </label>
-  <input type="submit" class="search-submit" value="Search">
-</form>
