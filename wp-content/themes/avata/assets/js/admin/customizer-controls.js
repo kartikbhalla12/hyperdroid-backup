1e26e1d5c129277059ead336d8a12f074f0596b2
Kartik B <kartikbhalla12@gmail.com>
2017-12-31 08:06:49 +0000

    Automated Backup 31-12-2017
    Signed-off-by: Kartik B <kartikbhalla12@gmail.com>

---
+++ wp-content/themes/avata/assets/js/admin/customizer-controls.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function($) {
+	var avata_customize_scroller = function ( $ ) {
+    'use strict';
+
+    $( function () {
+        var customize = wp.customize;
+
+        $('ul[id*="avata_frontpage_sections_panel"] .accordion-section').not('.panel-meta').each( function () {
+            $( this ).on( 'click', function() {
+                var section = $( this ).attr('aria-owns').replace('sub-accordion-section-sidebar-widgets-section-','');
+                customize.previewer.send('clicked-customizer-section', section);
+            });
+        });
+    } );
+};
+
+avata_customize_scroller( jQuery );
+
+});
---
+++ wp-content/themes/siteorigin-unwind/inc/contributors.php
@@ -0,0 +1,52 @@
+<?php
+return json_decode( '{
+	"85ce7a450a7ee4895f3bd823505e2e12": {
+		"name": "adiraomj",
+		"email": "85ce7a450a7ee4895f3bd823505e2e12",
+		"loc": 9384,
+		"score": 491.58522793754776,
+		"percent": 70.70933754025866
+	},
+	"b7122312e4008f8f2c76911080bca01a": {
+		"name": "Andrew Misplon",
+		"email": "b7122312e4008f8f2c76911080bca01a",
+		"loc": 4002,
+		"score": 201.01822193326615,
+		"percent": 28.914345872548697
+	},
+	"d203744279876c5eda043108e2611648": {
+		"name": "Alex S",
+		"email": "d203744279876c5eda043108e2611648",
+		"loc": 10,
+		"score": 0.9236520646281363,
+		"percent": 0.13285758378371157
+	},
+	"a885c7bd5442dd2acd8c3e42001332c6": {
+		"name": "Alex S",
+		"email": "a885c7bd5442dd2acd8c3e42001332c6",
+		"loc": 5,
+		"score": 0.8259782479993606,
+		"percent": 0.11880823796055585
+	},
+	"36639e6e074b731b093bde5a77305179": {
+		"name": "Braam Genis",
+		"email": "36639e6e074b731b093bde5a77305179",
+		"loc": 42,
+		"score": 0.5430357275836586,
+		"percent": 0.07810994793157415
+	},
+	"43c0e7220c23bd6636cf5469be6e9612": {
+		"name": "Braam Genis",
+		"email": "43c0e7220c23bd6636cf5469be6e9612",
+		"loc": 7,
+		"score": 0.3204448460321919,
+		"percent": 0.046092603059270654
+	},
+	"367746dddf905c98fc440395f0fc97f1": {
+		"name": "Rasmus Jürs",
+		"email": "367746dddf905c98fc440395f0fc97f1",
+		"loc": 1,
+		"score": 0.0031160751030828854,
+		"percent": 0.0004482144575196146
+	}
+}', true );
