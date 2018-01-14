4e454c4d044477da05f9504eaedd42aac1d2901e
Kartik B <kartikbhalla12@gmail.com>
2018-01-14 08:21:02 +0000

    Automated Backup 14-01-2018
    Signed-off-by: Kartik B <kartikbhalla12@gmail.com>

--- wp-content/themes/big-pix/header.php
+++ wp-content/themes/big-pix/header.php
@@ -24,12 +24,12 @@
 		
 		<?php wp_head(); ?>
 		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-  (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: "ca-pub-3764796401079806",
-    enable_page_level_ads: true
-  });
-</script>
+		<script>
+			(adsbygoogle = window.adsbygoogle || []).push({
+				google_ad_client: "ca-pub-3764796401079806",
+				enable_page_level_ads: true
+			});
+		</script>
 	</head>
 
 	<body <?php body_class(); ?> >
